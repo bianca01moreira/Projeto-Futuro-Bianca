5a6192fd6cfdc541578dfd2d2e2d4ddebd62f60c
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2025-12-27 22:36:20 -0300

    Melhorando a tela de Inicio

--- ProjetoCursos/src/Paginas/telaInicio.jsx
+++ ProjetoCursos/src/Paginas/telaInicio.jsx
@@ -1,10 +1,32 @@
 
+let estilo = {
+    height:"100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#0055A0",
+}
+let estiloBotao = {
+    backgroundColor:"#FFFFFF",
+    width:"400px",
+    height:"400px",
+    border: "none",
+    borderRadius:"10px"
+}
+let estiloAlinhamento = {
+    display: "flex",
+    justifyContent: "space-evenly",
+    width:"80vw"
+}
 
-function telaInicio(){
+function TelaInicio(){
     return(
-        <>
-            <button>Iniciar Jornada</button>
-            <button>Criar Curso</button>
-        </>
+        <div style={estilo}>
+            <div style={estiloAlinhamento}>
+                <button style={estiloBotao}>Iniciar Jornada</button>
+                <button style={estiloBotao}>Criar Curso</button>
+           </div>
+        </div>
     )
 }
+export default TelaInicio;
