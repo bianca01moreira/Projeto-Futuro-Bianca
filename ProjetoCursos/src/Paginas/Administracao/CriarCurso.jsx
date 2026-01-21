63f61b68c6b1d9f47bf4bca5a2b4d63075e09e72
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-01-21 20:42:15 -0300

    Melhorei as telas de Criar e dar curso

--- ProjetoCursos/src/Paginas/Administracao/CriarCurso.jsx
+++ ProjetoCursos/src/Paginas/Administracao/CriarCurso.jsx
@@ -1,8 +1,35 @@
+import logo from '../../assets/imagens/logo.png';
+
 function CriarCurso(){
+    let estiloFundo = {
+        minHeight:"100vh",
+        backgroundColor: "#F4F4F4",
+    }
+    let estiloHeader = {
+        minHeight:"90px",  
+        backgroundColor:"#0055A0",
+    }
+    let estiloMenuLateral = {
+        minHeight:"100vh",
+        maxWidth:"30vh",
+        minWidth:"15vh",
+        backgroundColor:"#0055A0"
+    }
     return (
-    <>
-        Tela onde futuramente o adm criará os cursos
-    </>
+    <div style={estiloFundo}>
+        <div style={estiloHeader}>
+            <img 
+                src={logo} 
+                style={{
+                    maxWidth: "180px", 
+                    maxHeight:"150px", 
+                }}
+            />  
+        </div>
+        <div style={estiloMenuLateral}>
+
+        </div>
+    </div>
     )
 }
 export default CriarCurso;
--- ProjetoCursos/src/Paginas/DarCurso/IniciarJornada.jsx
+++ ProjetoCursos/src/Paginas/DarCurso/IniciarJornada.jsx
@@ -1,3 +1,5 @@
+import logo from '../../assets/imagens/logo.png';
+
 function IniciarJornada(){
 
     let estiloFundo = {
@@ -5,13 +7,20 @@ function IniciarJornada(){
         backgroundColor: "#F4F4F4",
     }
     let estiloHeader = {
-        minHeight:"100px",
+        minHeight:"90px",  
         backgroundColor: "#0055A0",
-        
     }
+   
     return (
     <div style={estiloFundo}>
         <div style={estiloHeader}>
+            <img 
+                src={logo} 
+                style={{
+                    maxWidth: "180px", 
+                    maxHeight:"150px", 
+                }}
+            />  
         </div>
     </div>
     )
