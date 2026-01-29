5b84370523768418df6cb17056c4f0230fbfa6c8
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-01-29 14:32:59 -0300

    criei hook e melhorei a tela

--- ProjetoCursos/src/Paginas/Administracao/CriarCurso.jsx
+++ ProjetoCursos/src/Paginas/Administracao/CriarCurso.jsx
@@ -1,15 +1,36 @@
 import Menu from "../../Menu.jsx"
+import {useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function CriarCurso(){
+
+    //hook
+    const[nomeCurso, setnomeCurso] = useState("Nome do Curso")
+    const[nivelCurso, setnivelCurso] = useState("Nível do curso")
+
+    const[moduloCurso, setModuloCurso] = useState("")
+    const[aula, setAula] = useState("")
+
     let estiloMenuLateral = {
-        minHeight:"calc(100vh - 155px)",
+        minHeight:"calc(100vh - 150px)",
         maxWidth:"30vh",
         minWidth:"15vh",
-        backgroundColor:"#0055A0"
+        backgroundColor:"#0055A0",
+        display: "flex",
+        justifyContent:"space-evenly",
+        flexDirection:"column"
+    }
+    let estiloBotao = {
+        border: "none",
+        backgroundColor:"#0055A0",
     }
     return (
     <div>
-        <Menu>aa</Menu>
+        <Menu>
+            <span>{nomeCurso}</span>
+            <span>{nivelCurso}</span>
+        </Menu>
         <div style={estiloMenuLateral}>
+            <button style={estiloBotao}><FontAwesomeIcon icon="fa-solid fa-circle-plus" size="3x" style={{color: "#f7db12",}} /></button>
         </div>
     </div>
     )
