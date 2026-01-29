91de20af8ff03d4114b6fae403a17852913d2640
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-01-29 14:32:06 -0300

    arrumei o espaçamento com display flex

--- ProjetoCursos/src/Menu.jsx
+++ ProjetoCursos/src/Menu.jsx
@@ -3,6 +3,13 @@ function Menu({children}) {
     let estiloHeader = {
         minHeight:"150px",  
         backgroundColor:"#0055A0",
+        display:"flex",
+        justifyContent: 'space-between',
+        alignItems: "center",
+        paddingRight:"40px",
+        paddingLeft:"20px",
+        color:"#FCFEFF",
+        fontSize:"20px"
     }
     return(
         <div style={estiloHeader}>
