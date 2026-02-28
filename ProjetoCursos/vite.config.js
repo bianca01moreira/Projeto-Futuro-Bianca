c33595f2ca97b41e95b43563b2f59d3439ea6df5
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-02-27 21:53:00 -0300

    mais alguns ajustes

--- ProjetoCursos/vite.config.js
+++ ProjetoCursos/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@fortawesome/fontawesome-svg-core': '@fortawesome/fontawesome-svg-core',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      supported: {
+        bigint: true,
+      },
+    },
+  },
 })
