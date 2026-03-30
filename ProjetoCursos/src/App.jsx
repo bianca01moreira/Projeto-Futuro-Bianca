2e70e291127fefbd7bf80ea5e28518106a842a01
Bianca Farias Moreira <bianca01.moreira@gmail.com>
2026-03-30 11:19:59 -0300

    criei a tela onde dará para verificar as solicitações dos usuários

--- ProjetoCursos/src/App.jsx
+++ ProjetoCursos/src/App.jsx
@@ -5,6 +5,7 @@ import SolicitarAcesso from "./Paginas/SolicitarAcesso"
 import TelaInicio from "./Paginas/telaInicio"
 import CriarCurso from "./Paginas/Administracao/CriarCurso"
 import IniciarJornada from "./Paginas/DarCurso/IniciarJornada"
+import Solicitacoes from "./Paginas/Administracao/Solicitacoes"
 
 function App() {
   let estiloFundo = {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/telaInicio" element={<TelaInicio/>}/>
         <Route path="/CriarCurso" element={<CriarCurso/>} />
         <Route path="IniciarJornada" element={<IniciarJornada/>}/>
+        <Route path="/Solicitacoes" element={<Solicitacoes/>}/>
       </Routes>
     </div>
   )
---
+++ ProjetoCursos/src/Paginas/Administracao/Solicitacoes.jsx
@@ -0,0 +1,11 @@
+///Aqui vão estar as solicitações que serão realizadas
+function Solicitacoes(){
+    return(
+        <>
+            Tela para mostar as solicitações dos usuários
+
+        </>
+    )
+
+}
+export default Solicitacoes;
